28c0461402a015d2dfe8bb43ac1917fddfb4df1f
Kamran Qurbanlı <k.qurbanli@gmail.com>
2024-10-24 18:08:02 +0300

    Improve mailer.go

--- verification/mailer.go
+++ verification/mailer.go
@@ -4,12 +4,13 @@ import (
 	"crypto/tls"
 	"fmt"
 	gomail "gopkg.in/mail.v2"
+	"os"
 )
 
 func SendEmail(email string, hdr string, msg string) (string, error) {
 
-	username := "info@greatstuff.ee"
-	password := "bta3stW&"
+	username := os.Getenv("GREATSTUFF_CONTACT_EMAIL")
+	password := os.Getenv("GREATSTUFF_CONTACT_PASSWORD")
 
 	smtpHost := "smtp.zoho.eu"
 	smtpPort := 465
@@ -17,20 +18,14 @@ func SendEmail(email string, hdr string, msg string) (string, error) {
 	message := msg
 
 	m := gomail.NewMessage()
-
 	m.SetHeader("From", username)
-
-	m.SetHeader("To", "info@greatstuff.ee")
-
+	m.SetHeader("To", email)
 	m.SetHeader("Subject", hdr)
-
 	m.SetBody("text/plain", message)
 
 	d := gomail.NewDialer(smtpHost, smtpPort, username, password)
-
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
-	// Now send E-Mail
 	if err := d.DialAndSend(m); err != nil {
 		return err.Error(), err
 	} else {
